5b3e6a825dba1d0de5f34bf923d64ee529092caf
Christina <christina.barker.35@gmail.com>
2019-02-01 20:45:57 -0800

    dom-pt1-padding:completed

--- app.js
+++ app.js
@@ -1,48 +1,34 @@
-
 function onReady() {
-  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
-  function renderTheUI(){
-    const toDoList = document.getElementById('toDoList');
+   addToDoForm.addEventListener('submit', event => {
+     event.preventDefault();
 
-    toDoList.textContent = '';
+     let title = newToDoText.value;
 
-    toDos.forEach(function(toDo) {
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
+     let newLi = document.createElement('li');
+     let checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
 
+     let deleteBtn = document.createElement("button");
+     deleteBtn.textContent = "Delete";
 
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi)
-      newLi.appendChild(checkbox);
-    });
-
-  }
-
-  function  createNewToDo(){
-    const newToDoText = document.getElementById('newToDoText');
-    toDos.push ({
-      title: newToDoText.value,
-      complete: false,
-    });
-
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-    createNewToDo();
+     deleteBtn.addEventListener('click', function(event) {
+        toDoList.removeChild(this.parentElement);
   });
 
-  renderTheUI();
+     newLi.textContent = title;
+     newLi.appendChild(checkbox);
+     toDoList.appendChild(newLi);
+     newToDoText.value = '';
+
+     newLi.appendChild(deleteBtn);
+  });
 };
 
 window.onload= function () {
-  alert("Hi!");
+  alert("hi!");
   onReady();
 };
